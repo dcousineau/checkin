4738ba9080f1af3dd1ddb9d236771abed26f9fde
Daniel Cousineau <dcousineau@gmail.com>
2017-04-16 17:33:56 -0400

    Even cleaner syntax

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -7,35 +7,31 @@ import loadHome from 'bundle-loader?lazy!../pages/home';
 import loadAdmin from 'bundle-loader?lazy!../pages/admin';
 import PageNotFound from '../pages/pagenotfound';
 
-const lazy = loader => {
-    class LazyLoader extends React.Component {
-        state = {
-            Component: null,
-        };
+const lazy = loader => class extends React.Component {
+    state = {
+        Component: null,
+    };
 
-        componentWillMount() {
-            loader(mod => this.setState({
-                Component: mod.default ? mod.default : mod,
-            }));
-        }
-        
-        render() {
-            const {Component} = this.state;
+    componentWillMount() {
+        loader(mod => this.setState({
+            Component: mod.default ? mod.default : mod,
+        }));
+    }
+    
+    render() {
+        const {Component} = this.state;
 
-            if (Component !== null) {
-                return <Component {...this.props} />;
-            } else {
-                return (
-                    <div className="center-center-loader">
-                        <CircularProgress className="center-center" />
-                    </div>
-                );
-            }
+        if (Component !== null) {
+            return <Component {...this.props} />;
+        } else {
+            return (
+                <div className="center-center-loader">
+                    <CircularProgress className="center-center" />
+                </div>
+            );
         }
     }
-
-    return LazyLoader;
-}
+};
 
 export default class AppContainer extends React.Component {
     render() {
