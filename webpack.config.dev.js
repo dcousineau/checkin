fea8d97c01b8ae635924bf21ce5c5247c881bcd4
Daniel Cousineau <dcousineau@gmail.com>
2017-04-21 14:55:51 -0500

    Run prettier against webpack configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,90 +1,93 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
 
 module.exports = {
-    name: 'app',
-    devtool: 'eval-source-map', //devtool: 'eval' for maximum build performance
-    entry: {
-        vendors: [
-            'react-hot-loader/patch',
-            'webpack-dev-server/client?http://localhost:3001/',
-            './src/client/__vendors__.js',
-        ],
-        app: './src/client/__app__.js',
-    },
-    output: {
-        path: path.join(__dirname, 'build/static'),
-        publicPath: "/static/",
-        filename: "js/[name].js"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("development")
-            }
-        }),
-
-        new StatsWriterPlugin({
-            filename: "assets.json"
-        }),
-
-        //The minChunks function ensures vendors only includes those files that are produced from 3rd party vendors,
-        //namely anything in the node_modules folder
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "vendors",
-            minChunks: function(module){
-                return module.context && module.context.indexOf("node_modules") !== -1;
-            }
-        }),
-        
-        //The minChunks: Infinity creates a separate chunk, `manifest`, that contains only bootstrapping information
-        //required by Webpack compiled modules
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "manifest",
-            minChunks: Infinity
-        }),
-
-        new webpack.HotModuleReplacementPlugin(),
-
-        new webpack.NamedModulesPlugin(),
-
-        // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
-        new webpack.NoEmitOnErrorsPlugin(),
+  name: "app",
+  devtool: "eval-source-map", //devtool: 'eval' for maximum build performance
+  entry: {
+    vendors: [
+      "react-hot-loader/patch",
+      "webpack-dev-server/client?http://localhost:3001/",
+      "./src/client/__vendors__.js"
     ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: [
-                    'react-hot-loader/webpack',
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            babelrc: false,
-                            presets: [["es2015", {"modules": false}], "react", "stage-0"],
-                            plugins: [
-                                "react-hot-loader/babel",
-                                ["transform-runtime", {
-                                    "polyfill": true,
-                                    "regenerator": true
-                                }],
-                                "transform-react-jsx-source"
-                            ],
-                        }
-                    }
+    app: "./src/client/__app__.js"
+  },
+  output: {
+    path: path.join(__dirname, "build/static"),
+    publicPath: "/static/",
+    filename: "js/[name].js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+
+    new StatsWriterPlugin({
+      filename: "assets.json"
+    }),
+
+    //The minChunks function ensures vendors only includes those files that are produced from 3rd party vendors,
+    //namely anything in the node_modules folder
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendors",
+      minChunks: function(module) {
+        return module.context && module.context.indexOf("node_modules") !== -1;
+      }
+    }),
+
+    //The minChunks: Infinity creates a separate chunk, `manifest`, that contains only bootstrapping information
+    //required by Webpack compiled modules
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "manifest",
+      minChunks: Infinity
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+
+    new webpack.NamedModulesPlugin(),
+
+    // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          "react-hot-loader/webpack",
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              presets: [["es2015", { modules: false }], "react", "stage-0"],
+              plugins: [
+                "react-hot-loader/babel",
+                [
+                  "transform-runtime",
+                  {
+                    polyfill: true,
+                    regenerator: true
+                  }
                 ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.json$/,
-                loaders: ['json-loader']
+                "transform-react-jsx-source"
+              ]
             }
-        ]
-    },
-    devServer: {
-        hot: true,
-        compress: true,
-        port: 3001
-    }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loaders: ["json-loader"]
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    compress: true,
+    port: 3001
+  }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,76 +1,80 @@
 const base = require("./webpack.config.dev.js");
 
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
 
-
 module.exports = Object.assign({}, base, {
-    name: 'app',
-    devtool: false,
-    entry: Object.assign({}, base.entry, {
-        vendors: './src/client/__vendors__.js',
+  name: "app",
+  devtool: false,
+  entry: Object.assign({}, base.entry, {
+    vendors: "./src/client/__vendors__.js"
+  }),
+  output: Object.assign({}, base.output, {
+    filename: "js/[name].[chunkhash].min.js"
+  }),
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production") //Critical for dead-code elimination of debugging code in React
+      }
     }),
-    output: Object.assign({}, base.output, {
-        filename: "js/[name].[chunkhash].min.js"
+
+    new StatsWriterPlugin({
+      filename: "assets.json"
     }),
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production") //Critical for dead-code elimination of debugging code in React
-            }
-        }),
 
-        new StatsWriterPlugin({
-            filename: "assets.json"
-        }),
+    //The minChunks function ensures vendors only includes those files that are produced from 3rd party vendors,
+    //namely anything in the node_modules folder
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendors",
+      minChunks: function(module) {
+        return module.context && module.context.indexOf("node_modules") !== -1;
+      }
+    }),
 
-        //The minChunks function ensures vendors only includes those files that are produced from 3rd party vendors,
-        //namely anything in the node_modules folder
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "vendors",
-            minChunks: function(module){
-                return module.context && module.context.indexOf("node_modules") !== -1;
-            }
-        }),
-        
-        //The minChunks: Infinity creates a separate chunk, `manifest`, that contains only bootstrapping information
-        //required by Webpack compiled modules
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "manifest",
-            minChunks: Infinity
-        }),
-        
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                dead_code: true,
-                screw_ie8: true,
-                warnings: false,
-            }
-        }),
+    //The minChunks: Infinity creates a separate chunk, `manifest`, that contains only bootstrapping information
+    //required by Webpack compiled modules
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "manifest",
+      minChunks: Infinity
+    }),
 
-        // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
-        new webpack.NoEmitOnErrorsPlugin(),
-    ],
-    module: Object.assign({}, base.module, {
-        rules: [{
-            test: /\.jsx?$/,
-            use: [
-                {
-                    loader: "babel-loader",
-                    options: {
-                        babelrc: false,
-                        presets: [["es2015", {"modules": false}], "react", "stage-0"],
-                        plugins: [
-                            ["transform-runtime", {
-                                "polyfill": true,
-                                "regenerator": true
-                            }]
-                        ],
-                    }
-                }
-            ],
-            exclude: /node_modules/
-        }].concat(base.module.rules.slice(1))
-    })
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        dead_code: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    }),
+
+    // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: Object.assign({}, base.module, {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              presets: [["es2015", { modules: false }], "react", "stage-0"],
+              plugins: [
+                [
+                  "transform-runtime",
+                  {
+                    polyfill: true,
+                    regenerator: true
+                  }
+                ]
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ].concat(base.module.rules.slice(1))
+  })
 });
